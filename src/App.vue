b89e0880e3cb2be9af2c2f65d0ec02aac51776bd
Morar Sergiu <sergiu.morar7@gmail.com>
2018-08-13 11:51:31 +0300

    initial version

---
+++ src/App.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="container">
+    <i class="fas fa-angle-left" @click="prev"></i>
+    <!-- <button class="btn btn-prev" @click="prev">Previous Image</button> -->
+    <app-slide :index="index"></app-slide>
+    <i class="fas fa-angle-right" @click="next"></i>
+    <!-- <button class="btn btn-next" @click="next">Next image</button> -->
+  </div>
+</template>
+
+<script>
+  import Slide from './components/Slide.vue'
+
+  export default {
+    data() {
+      return {
+        index: 1
+      }
+    },
+    methods: {
+      next() {
+        if (this.index == 6) {
+          this.index = 1;
+        } else {
+          this.index ++;
+        }
+      },
+      prev() {
+        if (this.index == 1) {
+          this.index = 6;
+        } else {
+          this.index --;
+        }
+      }
+    },
+    components: {
+      appSlide: Slide
+    }
+  }
+
+</script>
+
+<style>
+
+  .container {
+    display: flex;
+    width: 1200px;
+    margin: 0 auto;
+    position: relative;
+  }
+
+  .fas {
+    font-size: 50px;
+    color: rgb(47, 141, 210);
+    cursor: pointer;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
+  .fa-angle-left {
+    left: 0;
+  }
+
+  .fa-angle-right {
+    right: 0;
+  }
+
+  .flip-enter-active {
+    animation: flip-in 1.5s linear forwards;
+  }
+
+  .flip-leave-active {
+    animation: flip-out 1.5s linear forwards;
+  }
+
+  @keyframes flip-out {
+    from {
+      opacity: 1;
+      transform: rotateY(0deg);
+    }
+    to {
+      opacity: 0.3;
+      transform: rotateY(45deg);
+    }
+  }
+
+  @keyframes flip-in {
+    from {
+      opacity: 0.3;
+      transform: rotateY(-45deg);
+    }
+    to {
+      opacity: 1;
+      transform: rotateY(0deg);
+    }
+  }
+</style>
